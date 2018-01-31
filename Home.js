ad3eb285510abcc7d8cce5fbca467c0a19d4cae5
isha2510 <ishatripathi80@gmail.com>
2018-01-31 22:05:42 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+          Home
+     
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ Transaction.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+class Transaction extends Component {
+  render() {
+    return (
+      <div>
+          Transaction
+     
+      </div>
+    );
+  }
+}
+
+export default Transaction;
